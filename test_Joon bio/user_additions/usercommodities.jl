7f75ad1764b3f42cbb5aa73a1e99e98fa7dd7b78
Joon Moon <jm6409@princeton.edu>
2026-04-14 16:53:33 -0400

    Rechecking balance constraint setting for example systems

---
+++ test_Joon bio/user_additions/usercommodities.jl
@@ -0,0 +1,4 @@
+abstract type Corn <: MacroEnergy.Biomass end
+abstract type Bagasse <: MacroEnergy.Biomass end
+abstract type Residue <: MacroEnergy.Biomass end
+abstract type Ethanol <: MacroEnergy.LiquidFuels end
