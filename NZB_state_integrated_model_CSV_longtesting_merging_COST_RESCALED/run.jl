848c23d8b3fabff46e2a72ebe366c6cad628ffa8
NZ-Brazil <nzb@princeton.edu>
2026-05-18 17:44:01 -0300

    Update run.jl

--- NZB_state_integrated_model_CSV_longtesting_merging_COST_RESCALED/run.jl
+++ NZB_state_integrated_model_CSV_longtesting_merging_COST_RESCALED/run.jl
@@ -12,8 +12,8 @@ using Gurobi
     # optimizer_attributes=(
     #     "Method" => 2,
     #     "Crossover" => 0,
-    #     "NumericFocus" => 3,
-    #     "BarConvTol" => 1e-4,
+    #     "NumericFocus" => 2,
+    #     "BarConvTol" => 1e0,
     #     "BarHomogeneous" => 1,
     #     "ScaleFlag" => 2,           
     #     "ObjScale" => -1,           
