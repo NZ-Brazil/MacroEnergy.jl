2a7e4f51045623114f621e5ec0bc87917fad93f7
Rachel Tamburini <tamburini.rg@gmail.com>
2026-08-15 13:36:16 -0300

    Delete src directory

--- src/model/types/supply.jl
+++
@@ -1,8 +0,0 @@
-Base.@kwdef struct SupplySegment
-    price::Vector{Float64}
-    min::Vector{Float64}
-    max::Vector{Float64}
-end
-
-Base.:(==)(left::SupplySegment, right::SupplySegment) =
-    left.price == right.price && left.min == right.min && left.max == right.max
